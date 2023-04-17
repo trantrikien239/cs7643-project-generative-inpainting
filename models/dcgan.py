75877143d44a701bf5b9546277a3651506f44be3
Tanmay Kenjale <thkenjale@gmail.com>
2023-04-17 17:04:41 -0400

    DCGAN init

---
+++ models/dcgan.py
@@ -0,0 +1,104 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import transforms, models
+from torch.utils.data import Dataset, DataLoader
+
+cuda = True if torch.cuda.is_available() else False
+
+batch_size = 128 # Batch size during training
+image_size = 256 # Spatial size of training images
+nc = 3 # Number of channels in the training images. For color images this is 3
+nz = 100 # Size of z latent vector (i.e. size of generator input)
+ngf = 64 # Size of feature maps in generator
+ndf = 64 # Size of feature maps in discriminator
+epochs = 5 # Number of training epochs
+lr = 0.0002 # Learning rate for optimizers
+beta1 = 0.5 # Beta1 hyperparameter for Adam optimizers
+ngpu = 1 if cuda else 0 # Number of GPUs available. Use 0 for CPU mode.
+
+device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
+
+class Generator(nn.Module):
+    def __init__(self, ngpu):
+        super(Generator, self).__init__()
+
+        self.ngpu = ngpu
+        self.conv = nn.Sequential(
+            # 100 x 1 x 1
+            nn.ConvTranspose2d(in_channels=nz, out_channels=1024, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(1024),
+            nn.ReLU(inplace=True),
+            # 1024 x 4 x 4
+            nn.ConvTranspose2d(in_channels=1024, out_channels=512, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            # 512 x 8 x 8
+            nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            # 256 x 16 x 16
+            nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            # 128 x 32 x 32
+            nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            # 64 x 64 x 64
+            nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            # 32 x 128 x 128
+            nn.ConvTranspose2d(in_channels=32, out_channels=3, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.Tanh()
+            # 3 x 256 x 256
+        )
+
+    def forward(self, input):
+        return self.conv(input)
+    
+    
+class Discriminator(nn.Module):
+    def __init__(self, ngpu):
+        super(Discriminator, self).__init__()
+        self.ngpu = ngpu
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=ndf, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(in_channels=ndf, out_channels=ndf*2, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ndf*2),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(in_channels=ndf*2, out_channels=ndf*4, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ndf*4),
+            nn.LeakyReLU(0.2, inplace=True),
+          
+            nn.Conv2d(in_channels=ndf*4, out_channels=ndf*8, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ndf*8),
+            nn.LeakyReLU(0.2, inplace=True),
+            
+            nn.Conv2d(in_channels=ndf*8, out_channels=ndf*16, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ndf*16),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(in_channels=ndf*16, out_channels=ndf*32, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ndf*32),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(in_channels=ndf*32, out_channels=1, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.Sigmoid()
+        )
+    def forward(self, input):
+        return self.conv(input)
